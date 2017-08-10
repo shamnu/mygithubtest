7e08405c158b9fbee065019aad3db0bff242d009
shamnadmh007@gmail.com <shamnadmh007@gmail.com>
2017-08-10 18:55:39 +0530

    Added Folder and model project on 8-10-2017

--- BaseFramework/BaseFramework/BaseFramework/MainPage.xaml.cs
+++ BaseFramework/BaseFramework/BaseFramework/MainPage.xaml.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using Models;
 namespace BaseFramework
 {
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
+            
         }
     }
 }
